5313b5d21f97363dc6c145efb9cb53d799affd8e
Allan Nava <allannava95@gmail.com>
2020-05-26 17:21:31 +0200

    Update bloc-event-state.template.ts

--- src/templates/bloc-event-state.template.ts
+++ src/templates/bloc-event-state.template.ts
@@ -33,34 +33,29 @@ import 'package:rxdart/rxdart.dart';
 abstract class VanillaBlocEvent extends Object {}
 abstract class VanilleBlocState extends Object {}
 
-abstract class VanilleBlocEventStateBase<VanillaBlocEvent, VanillaBlocState> implements VanillaBlocBase {
+abstract class VanillaBlocEventStateBase<VanillaBlocEvent, VanillaBlocState> implements VanillaBlocBase {
   PublishSubject<VanillaBlocEvent> _eventController = PublishSubject<VanillaBlocEvent>();
   BehaviorSubject<VanillaBlocState> _stateController = BehaviorSubject<VanillaBlocState>();
-
   ///
   /// To be invoked to emit an event
   ///
   Function(VanillaBlocEvent) get emitEvent => _eventController.sink.add;
-
   ///
   /// Current/New state
   ///
   Stream<VanillaBlocState> get state => _stateController.stream;
-
   ///
   /// External processing of the event
   ///
   Stream<VanillaBlocState> eventHandler(VanillaBlocEvent event, VanillaBlocState currentState);
-
   ///
   /// initialState
   ///
   final VanillaBlocState initialState;
-
   //
   // Constructor
   //
-  VanilleBlocEventStateBase({
+  VanillaBlocEventStateBase({
     @required this.initialState,
   }){
     //
@@ -74,7 +69,6 @@ abstract class VanilleBlocEventStateBase<VanillaBlocEvent, VanillaBlocState> imp
       });
     });
   }
-
   @override
   void dispose() {
     _eventController.close();
