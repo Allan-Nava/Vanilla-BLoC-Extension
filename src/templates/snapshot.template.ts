e086f6106711ed7a87c43f1c09900efc591d01ca
Allan Nava <allannava95@gmail.com>
2020-07-09 14:17:48 +0200

    Update snapshot.template.ts

--- src/templates/snapshot.template.ts
+++ src/templates/snapshot.template.ts
@@ -48,18 +48,17 @@ class SnapshotHelper<T> {
       {SnapshotBuilder<T> onData,
       SnapshotBuilder<T> onError,
       SnapshotBuilder<T> onLoading}) {
-    if (snapshot.connectionState == ConnectionState.waiting) {
+    if (snapshot.hasError) {
+      return onError(snapshot);
+    } else if (snapshot.connectionState == ConnectionState.waiting) {
       return onLoading(snapshot);
     } else if (snapshot.connectionState == ConnectionState.active &&
         !snapshot.hasData) {
       return onLoading(snapshot);
     } else if (snapshot.hasData && !snapshot.hasError) {
       return onData(snapshot);
-    } else if (snapshot.hasError) {
-      return onError(snapshot);
     }
     return null;
-  }
 }
 
 //usage:
